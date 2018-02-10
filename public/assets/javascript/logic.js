63cbaeaec45a7dab69697e907ad0cf207d1e9b93
Good Beard <goodbeard@Goods-MacBook-Pro.local>
2018-02-10 10:10:04 -0500

    fixed some js issues

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -20,5 +20,26 @@ $(document).ready(function() {
     	})
     }
 
+	$(".create-form").on("submit", function(event) {
+	    // Make sure to preventDefault on a submit event.
+	    event.preventDefault();
+	    var newBurger = {
+	       	burger_name: $("#burger").val().trim()
+	                
+	        };
+			console.log("added: " + newBurger)
+	        // Send the POST request.
+	        $.ajax("/api/burger", {
+	            type: "POST",
+	            data: newBurger
+	        }).then(
+	            function() {
+	                console.log("created new burger");
+	                // Reload the page to get the updated list
+	                location.reload();
+	            }
+	        );
+	    });
+
 
 });
